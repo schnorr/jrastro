b5e3cdd02856eed64ad428b1e0ec1286ea158838
schnorr <Lucas.Schnorr@imag.fr>
2012-09-14 22:06:20 +0200

    applying correct GPL3 license to all files

--- examples/SimpleThread.java
+++ examples/SimpleThread.java
@@ -1,3 +1,20 @@
+/*
+    This file is part of jrastro.
+
+    jrastro is free software; you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    jrastro is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+    for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with jrastro; if not, write to the Free Software Foundation, Inc.,
+	51 Franklin Street, Fifth Floor, Boston, MA 02111 USA.
+*/
 class SimpleThread extends Thread {
     public SimpleThread(String str) {
 	super(str);
